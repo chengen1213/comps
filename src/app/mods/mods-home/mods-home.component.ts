9caf9cf0e00f4e6fa7764ddd91acdeeb5736bcb1
En <a0955105@gmail.com>
2020-05-06 09:20:01 +0800

    mods

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -3,15 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mods-home',
   templateUrl: './mods-home.component.html',
-  styleUrls: ['./mods-home.component.css']
+  styleUrls: ['./mods-home.component.css'],
 })
 export class ModsHomeComponent implements OnInit {
   modalOpen = false;
+  items = [
+    { title: 'Q1', content: 'A1' },
+    { title: 'Q2', content: 'A2' },
+    { title: 'Q3', content: 'A3' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onClick() {
     this.modalOpen = !this.modalOpen;
